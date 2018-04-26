865dbda56891f5924b90e6f68b80169a34ec5c1f
notpace <notpace@gmail.com>
2018-04-26 18:42:24 -0400

    Add field for alternative evidence when a validation fails

--- app/models/failure.rb
+++ app/models/failure.rb
@@ -1,11 +1,12 @@
 # Failures (errors, warnings, etc.) that could be encountered during a test run
 class Failure
   include Mongoid::Document
-  field :id,            type: String
-  field :name,          type: String
-  field :description,   type: String
-  field :mitigation,    type: String
-  field :severity,      type: String
+  field :id,                    type: String
+  field :name,                  type: String
+  field :description,           type: String
+  field :mitigation,            type: String
+  field :severity,              type: String
+  field :alternative_evidence,  type: String
 
   belongs_to :validation_instance
 end
