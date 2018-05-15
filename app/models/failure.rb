468cbf398d182c1cce0c069031f33e41ce00e30f
Andrew Schreiber <schreiaj@gmail.com>
2018-05-15 16:58:57 -0400

    added proper mongoid timestamps to failure and validation instance

--- app/models/failure.rb
+++ app/models/failure.rb
@@ -1,11 +1,13 @@
 # Failures (errors, warnings, etc.) that could be encountered during a test run
 class Failure
   include Mongoid::Document
-  field :id,                    type: String
-  field :name,                  type: String
-  field :description,           type: String
-  field :mitigation,            type: String
-  field :severity,              type: String
+  include Mongoid::Timestamps
+
+  field :id,            type: String
+  field :name,          type: String
+  field :description,   type: String
+  field :mitigation,    type: String
+  field :severity,      type: String
 
   belongs_to :validation_instance
 end
--- app/models/validation_instance.rb
+++ app/models/validation_instance.rb
@@ -1,9 +1,10 @@
 # An individual run of one of the available validations on a given server
 class ValidationInstance
   include Mongoid::Document
+  include Mongoid::Timestamps
+
   field :state,           type: String
   field :actual_result,   type: String
-  field :created_at,      type: Time
   field :url,             type: String
 
   belongs_to :validation
